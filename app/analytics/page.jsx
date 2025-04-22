96528a077f8199504a8c94abdc3abb8bf36f50a3
daprince1110 <198153437+daprince1110@users.noreply.github.com>
2025-04-22 13:56:55 -0500

    New analysis msg

--- app/analytics/page.jsx
+++ app/analytics/page.jsx
@@ -1,7 +1,7 @@
 export default function(){
     return (
         <div>
-            <p>Now see analytics I guess</p>
+            <p>Now this is the part where we use AI to generate</p>
         </div>
     )
 }
